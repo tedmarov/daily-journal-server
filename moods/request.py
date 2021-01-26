61afd16149e84f0a715ac4f99d95b3a0ef5bc2ef
ted <tedmarov@gmail.com>
2021-01-26 00:05:33 -0600

    Deletes, searches entry field, embedded mood

--- moods/request.py
+++ moods/request.py
@@ -2,9 +2,9 @@ import sqlite3
 import json
 from models import Mood
 
-def get_all_animals():
+def get_all_moods():
     # Open a connection to the database
-    with sqlite3.connect("./kennel.db") as conn:
+    with sqlite3.connect("./dailyjournal.db") as conn:
 
         # Just use these. It's a Black Box.
         conn.row_factory = sqlite3.Row
@@ -19,11 +19,11 @@ def get_all_animals():
             a.status,
             a.location_id,
             a.customer_id
-        FROM animal a
+        FROM mood a
         """)
 
-        # Initialize an empty list to hold all animal representations
-        animals = []
+        # Initialize an empty list to hold all mood representations
+        moods = []
 
         # Convert rows of data into a Python list
         dataset = db_cursor.fetchall()
@@ -31,15 +31,15 @@ def get_all_animals():
         # Iterate list of data returned from database
         for row in dataset:
 
-            # Create an animal instance from the current row.
+            # Create an mood instance from the current row.
             # Note that the database fields are specified in
             # exact order of the parameters defined in the
-            # Animal class above.
-            animal = Animal(row['id'], row['name'], row['breed'],
+            # mood class above.
+            mood = mood(row['id'], row['name'], row['breed'],
                             row['status'], row['location_id'],
                             row['customer_id'])
 
-            animals.append(animal.__dict__)
+            moods.append(mood.__dict__)
 
     # Use `json` package to properly serialize list as JSON
-    return json.dumps(animals)
+    return json.dumps(moods)
